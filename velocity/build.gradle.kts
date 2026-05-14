80dc3731722a2faf73f8681407605f0f4b532556
LPZockerHD <90093334+Chwitst187@users.noreply.github.com>
2026-05-14 15:14:57 +0200

    build: update repository and dependencies for Velocity API to version 3.5.0-SNAPSHOT

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 repositories {
     maven {
-        name = "velocity-snapshots"
-        url = uri("https://repo.velocitypowered.com/snapshots/")
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
 dependencies {
-    compileOnly("com.velocitypowered:velocity-api:1.1.5")
-    annotationProcessor("com.velocitypowered:velocity-api:1.1.5")
+    compileOnly("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
+    annotationProcessor("com.velocitypowered:velocity-api:3.5.0-SNAPSHOT")
 }
